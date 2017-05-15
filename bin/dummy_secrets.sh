97e25d9deeb31075a28174d4cb93cf8b443e519e
Skorfulose <Skorfulose@users.noreply.github.com>
2017-05-15 22:39:35 +0200

    Add Google travel time sensors

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -2,3 +2,8 @@
 
 echo "home_latitude: 40.689227" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "home_longitude: -74.044512" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "google_maps_api_key: xxxxxxxxx" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "travel_friendly_name_1: Person 1 - To Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "travel_friendly_name_2: Person 1 - From Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "travel_friendly_name_3: Person 2 - To Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "travel_friendly_name_4: Person 2 - From Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
