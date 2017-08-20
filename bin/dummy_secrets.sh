dae0556412e159b03aac9ab77717198142fb7157
Skorfulose <Skorfulose@users.noreply.github.com>
2017-08-20 21:57:37 +0200

    Add Xiaomi Mi Vacuum Robot

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -7,3 +7,5 @@ echo "travel_friendly_name_1: Person 1 - To Work" >> $TRAVIS_BUILD_DIR/secrets.y
 echo "travel_friendly_name_2: Person 1 - From Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "travel_friendly_name_3: Person 2 - To Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "travel_friendly_name_4: Person 2 - From Work" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "mi_robo_ip: 127.0.0.1" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "mi_robo_token: 0123456789abcdef0123456789abcdef" >> $TRAVIS_BUILD_DIR/secrets.yaml
